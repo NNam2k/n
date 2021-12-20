2334b7204731ccc56f1c7690a6fc7ef1cc7ad3b6
NNam2k <96414468+NNam2k@users.noreply.github.com>
2021-12-20 20:07:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
     introTitle: 'Hmmm hi !!!',
     introDesc: `Khi ma cau bam vao cai link nay nghia la cau cung k co ai de di choi cung dung khumm?? `,
-    btnIntro: 'dung roi, dang ế mà mn :((((((  ',
+    btnIntro: 'đúng rồi, đang ế mà mn 🥲  ',
     title: 'Di choi cung nn ?? ',
     btnYes: 'Di chu tr oii <3 ',
     btnNo: 'Không, ham l a? ',
     question:'The may h nhi? ',
     btnReply: ' okiii ',
-    reply: '8h nhe aiu, <3',
-    mess: 'ỏ :33333 ',
+    reply: '8h nhe aiu <3',
+    mess: 'Ỏ oke ems :33333 ',
     messDesc: 'Tối mai 8h anh qua đón nhé công chúa, đ ra t xách cl đi :) .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100014594463973'
