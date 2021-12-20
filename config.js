f1f97e663b750e54056bf184b6b495d16ae509f4
NNam2k <96414468+NNam2k@users.noreply.github.com>
2021-12-20 16:42:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,4 +12,4 @@ const CONFIG = {
     mess: 'Anh biết mà :3 ',
     messDesc: 'Tối mai 8h anh qua đón nhé công chúa, d ra t xach cl di :) .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/' 
+    messLink: 'https://www.facebook.com/ngocnam.pham2009/' 
